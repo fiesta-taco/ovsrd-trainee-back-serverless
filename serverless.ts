f9d707c3b3e1704ce9a68c6f421fa6f1d38c77cc
oleksandr-yakov <alex.yakov1109@gmail.com>
2023-08-16 17:23:21 +0300

    add the BatchWriteItem to role db

--- serverless.ts
+++ serverless.ts
@@ -29,6 +29,7 @@ const serverlessConfiguration: AWS = {
           'dynamodb:PutItem',
           'dynamodb:UpdateItem',
           'dynamodb:DeleteItem',
+          'dynamodb:BatchWriteItem',
         ],
         Resource: [
           'arn:aws:dynamodb:eu-central-1:666398651410:table/*',
