ab7e08ebaceac918574a22ba88d1774b9c5973f3
ihorbilash <first2007.ib@gmail.com>
2023-08-17 17:13:44 +0300

    add new field

--- src/models/interfaces/Card.ts
+++ src/models/interfaces/Card.ts
@@ -4,4 +4,5 @@ export default interface Card {
     position: number;
     title: string;
     cardText: string;
+    imageURL:string;
 }
--- src/models/interfaces/CreateCard.ts
+++ src/models/interfaces/CreateCard.ts
@@ -3,4 +3,5 @@ export default interface CreateCard {
     position: number;
     title: string;
     cardText: string;
+    imageURL:string;
 }
